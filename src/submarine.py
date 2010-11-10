7cec5fe42f67a5011c2d64b902a3e3240d38a2e6
David Stern <vorporeal@gmail.com>
2010-11-10 01:44:35 -0500

    Wrote a basic Submarine class.

---
+++ src/submarine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from graphics import *
+import numpy as np
+import pygame as pg
+from pygame.locals import *
+
+class Sub():
+    # pos is a 2-element numpy array storing the position of the sub
+    #   as a vector.
+    def __init__(self, pos):
+        self.g = Graphics()
+        self.speed = np.array([0.5, 0.3])
+        self.spawn(pos)
+    
+    def spawn(self, pos):
+        # Set initial values for the sub.
+        self.health = 100
+        
+        self.pos = pos
+    
+    # amt is a 2-element numpy translation vector (array)
+    def move(self, amt):
+        # Normalize the movement vector.
+        norm = np.linalg.norm(amt)
+        if norm != 0:
+            amt /= np.linalg.norm(amt)
+        # Modify the position, taking into account the speed of the sub.
+        self.pos += amt * self.speed
+    
+    def draw(self):
+        pg.draw.circle(self.g.screen, pg.Color("cornflower blue"), self.pos, 12)
