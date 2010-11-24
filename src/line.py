38200ea1bd8ced12f348b54128696e4840b6a5d2
David Stern <vorporeal@gmail.com>
2010-11-23 20:52:58 -0500

    Added some useful attributes to Line; added a reflect function to Line (untested).

--- src/line.py
+++ src/line.py
@@ -15,8 +15,21 @@ class Line(object):
         
             p1, p2: numpy arrays representing endpoints of the line. """
         
+        if np.all(p1 == p2):
+            raise Exception("can't create a line with length 0!")
+        
         self.p1 = p1
         self.p2 = p2
+        
+        self.vec = p2 - p1
+        
+        if self.vec[0] != 0:
+            self.slope = self.vec[1] / self.vec[0]
+        else:
+            self.slope = None
+        
+        self.normal = np.array((-1 * self.vec[1], self.vec[0]))
+        self.normal /= np.linalg.norm(self.normal)
     
     def draw(self, surface, offset):
         """ Draws the line to the screen. """
@@ -29,5 +42,13 @@ class Line(object):
             color = self.color
         else:
             color = Line.default_color
-            
-        pg.draw.line(surface, color, np.int32(self.p1 + offset), np.int32(self.p2 + offset), 2)
+        
+        p1 = np.int32(self.p1 + offset)
+        p2 = np.int32(self.p2 + offset)
+        
+        pg.draw.line(surface, color, p1, p2, 2)
+    
+    def reflect(self, incident):
+        """ Reflects a vector off of this line. """
+        
+        return incident - 2 * np.dot(self.normal, incident) * self.normal
